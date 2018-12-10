3cb987f3becfe92d90702ed414a7b9b1272324a4
JungYoungsu <terun707@gmail.com>
2018-12-10 16:21:11 +0900

    samsungtv- add price, xml- bean 2 arg

--- src/polymorphism3/SamsungTV.java
+++ src/polymorphism3/SamsungTV.java
@@ -2,6 +2,7 @@ package polymorphism3;
 
 public class SamsungTV implements TV {
 	private Speaker speaker;
+	private int price;
 	
 	public SamsungTV() {
 		System.out.println("===> SamsungTV(1) 积己");
@@ -9,6 +10,11 @@ public class SamsungTV implements TV {
 	public SamsungTV(Speaker speaker) {
 		System.out.println("===> SamsungTV(2) 积己");
 		this.speaker = speaker;
+	}
+	public SamsungTV(Speaker speaker,int price) {
+		System.out.println("===> SamsungTV(2) 积己");
+		this.speaker = speaker;
+		this.price = price;
 	}
 	public void powerOn() {
 		System.out.println("SamsungTV---傈盔 囊促.");
