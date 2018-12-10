29d267a22ba8333154ccbad11c14cd02774e3925
JungYoungsu <terun707@gmail.com>
2018-12-10 14:15:42 +0900

    apply spring, beanfactory tv

--- src/polymorphism3/TVUser.java
+++ src/polymorphism3/TVUser.java
@@ -1,10 +1,15 @@
 package polymorphism3;
 
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
 public class TVUser {
 	public static void main(String[] args) {
-		BeanContainer container = new BeanContainer();
+		// 1. Spring 컨테이너를 생성(구동)한다. 
+		XmlBeanFactory container = new XmlBeanFactory(new ClassPathResource("applicationContext.xml"));
 		
-		TV tv = (TV) container.getBean(args[0]);
+		// 2. Spring 컨테이너로부터 테스트할 객체를 검색(Lookup)한다.
+		TV tv = (TV) container.getBean("tv");
 		tv.powerOn();
 		tv.volumeUp();
 		tv.volumeDown();
