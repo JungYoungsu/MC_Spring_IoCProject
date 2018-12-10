84a1c021c33ba58ef597aab43b0a949df6bbc0f2
JungYoungsu <terun707@gmail.com>
2018-12-10 14:18:56 +0900

    applicationContext apply

--- src/polymorphism3/TVUser.java
+++ src/polymorphism3/TVUser.java
@@ -1,14 +1,14 @@
 package polymorphism3;
 
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TVUser {
 	public static void main(String[] args) {
 		// 1. Spring 컨테이너를 생성(구동)한다. 
-		XmlBeanFactory container = new XmlBeanFactory(new ClassPathResource("applicationContext.xml"));
+		ClassPathXmlApplicationContext container = 
+			new ClassPathXmlApplicationContext("applicationContext.xml");
 		
-		// 2. Spring 컨테이너로부터 테스트할 객체를 검색(Lookup)한다.
+//		// 2. Spring 컨테이너로부터 테스트할 객체를 검색(Lookup)한다.
 		TV tv = (TV) container.getBean("tv");
 		tv.powerOn();
 		tv.volumeUp();
