9a656130f78824f82f1c253941aaa377bc69308e
JungYoungsu <terun707@gmail.com>
2018-12-11 11:04:17 +0900

    clean import

--- src/com/multicampus/biz/user/UserServiceImpl.java
+++ src/com/multicampus/biz/user/UserServiceImpl.java
@@ -2,9 +2,6 @@ package com.multicampus.biz.user;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 // 4. Service 구현 클래스
 public class UserServiceImpl implements UserService {
 	private UserDAO userDAO;
